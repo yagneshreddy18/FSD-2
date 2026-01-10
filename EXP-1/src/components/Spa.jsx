e2bad4a9552f19a1486f256523f7ed01a03dbd3b
yagnesh <vattiguntayagneshreddy@gmail.com>
2026-01-10 12:53:43 +0530

    Updated App.jsx and added SPA routing

---
+++ EXP-1/src/components/Spa.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+
+export default function SinglePageApp() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
