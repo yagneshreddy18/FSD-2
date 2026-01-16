834a9107cb9987ac68452b75e4d80d6adfdc6014
yagnesh <vattiguntayagneshreddy@gmail.com>
2026-01-16 11:37:35 +0530

    updated EXP-2

--- EXP-2/src/components/Button.jsx
+++ EXP-2/src/components/Button.jsx
@@ -1,26 +1,43 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Rating from '@mui/material/Rating';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function ButtonBasic() {
   return (
     <>
-    <Button size="small" variant='outlined'>Small</Button>
-    <Button size="medium" variant='outlined'>Medium</Button>
-    <Button size="large" variant='outlined'>Large</Button>
-    <br/>
-    <br/>
-    <Button size='small' variant='contained'>Small</Button>
-    <Button size="medium" variant='contained'>Medium</Button>
-    <Button size="large" variant='contained'>Large</Button>
-    <br/>
-    <br/>
-    <Button size='small' variant='text'>Small</Button>
-    <Button size="medium" variant='text'>Medium</Button>
-    <Button size="large" variant='text'>Large</Button>
-    <br/>
-    <br/>
-    <TextField label="Small" size="small" variant="outlined"/>
-    <TextField label="Medium" size="medium" variant="outlined" />
+      
+
+      {/* ===== Component 2 : Button ===== */}
+      <Button size="small" variant="contained">Small</Button>
+      <Button size="medium" variant="contained">Medium</Button>
+      <Button size="large" variant="contained">Large</Button>
+      <br /><br />
+
+      
+
+     
+      <TextField label="Small" size="small" variant="outlined" />
+      <TextField label="Medium" size="medium" variant="outlined" />
+      <br /><br />
+
+      
+      <Select defaultValue="" displayEmpty size="small">
+        <MenuItem value="">Select Option</MenuItem>
+        <MenuItem value="1">Option 1</MenuItem>
+        <MenuItem value="2">Option 2</MenuItem>
+      </Select>
+      <br /><br />
+
+      
+      <Rating value={3} />
+      <br /><br />
+
+  
+      <FormControlLabel control={<Checkbox />} label="Accept Terms" />
     </>
-  ) 
+  );
 }
